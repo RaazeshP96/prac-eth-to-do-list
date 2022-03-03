db086f65d475d3f514a459a1360dd7c6821c6c10
raazeshp96 <raazeshp96@gmail.com>
2022-03-03 10:15:06 +0545

    fix:initilize create task function

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -4,4 +4,17 @@ pragma solidity ^0.8.11;
 
 contract Todolist{
     uint public taskCount=0;
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public task;
+    constructor() {
+        createTask("DO CODING");
+    }
+    function createTask(string memory _content)public{
+        taskCount ++;
+        task[taskCount]=Task(taskCount,_content,false);
+    }
 }
