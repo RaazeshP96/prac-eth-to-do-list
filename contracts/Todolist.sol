9fdd8115f76d8ea203e65c19befbd6cd3b1b1a4b
raazeshp96 <raazeshp96@gmail.com>
2022-03-04 00:05:05 +0545

    feat:create contract

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -2,19 +2,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-contract Todolist{
-    uint public taskCount=0;
-    struct Task{
-        uint id;
-        string content;
-        bool completed;
-    }
-    mapping(uint => Task) public task;
-    constructor() {
-        createTask("DO CODING");
-    }
-    function createTask(string memory _content)public{
-        taskCount ++;
-        task[taskCount]=Task(taskCount,_content,false);
-    }
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor()  {
+    createTask("Check out dappuniversity.com");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
 }
